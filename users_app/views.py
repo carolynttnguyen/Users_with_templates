b228e6a7ebafb74441c19e9588d8f71d1cb45efd
carongyn <carolynttnguyen@gmail.com>
2020-11-24 14:09:17 -0800

    first commit

---
+++ users_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .models import users
+
+def index(request): 
+    context = {
+        "all_users": users.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def user_input(request):
+    users.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        age=request.POST['age']
+    )
+    return redirect('/')
